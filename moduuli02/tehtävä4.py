c3de160b24f0b08525bfcfe190e06db2e0ef5199
Ilhaam <ilhaamn1@gmail.com>
2023-08-24 20:00:09 +0300

    moduuli02 tehtävä4

---
+++ moduuli02/tehtävä4.py
@@ -0,0 +1,16 @@
+# Kirjoita ohjelma, joka kysyy kolme kokonaislukua.
+# Ohjelma tulostaa lukujen summan, tulon ja keskiarvon.
+
+a = int(input('a: '))
+b = int(input("b: "))
+c = int(input("c: "))
+
+summa = a+b+c
+tulo = a*b*c
+keskiarvo = a+b+c/3
+
+
+
+print('summa on ',summa)
+print("tulo on",tulo)
+print("keskiarvo on",keskiarvo)
