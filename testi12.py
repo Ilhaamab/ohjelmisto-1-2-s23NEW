700b42163ef890a17ffa6a301ec1b43b84130fc8
Ilhaam <ilhaamn1@gmail.com>
2023-09-11 19:57:31 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	moduuli4/tehtävä3.py

--- testi12.py
+++ testi12.py
@@ -1,17 +1,5 @@
-rahat = float(input("Anna rahamäärä: "))
-if rahat>=5:
-    print("Voit ostaa latten.")
 
-   # Kirjoita ohjelma, joka kysyy käyttäjältä ikää jatulostaa sen perusteella viestin
-
-"Olet täysi-ikäinen"
-# tai
-"Et ole vielä täysi-ikäinen".
-
-
-ika = int(input("Syötä ikäsi: "))
-
-if ika >= 18:
-print("Olet täysi-ikäinen.")
-else:
-print("Et ole vielä täysi-ikäinen.")
+#  testi
+nimi = input("Anna nimi:")
+syntymävuosi = input("Anna vuosi:")
+print(f'Hei {nimi}', olet syntynyt eli  "1990")
