7786f2f0ef5a939f078d1c31879f15fbffd0a210
Ilhaam <ilhaamn1@gmail.com>
2023-09-11 22:06:17 +0300

    moduuli06,tehtävä6.py

--- testi12.py
+++ testi12.py
@@ -1,5 +1,11 @@
 
 #  testi
-nimi = input("Anna nimi:")
-syntymävuosi = input("Anna vuosi:")
-print(f'Hei {nimi}', olet syntynyt eli  "1990")
+#nimi = input("Anna nimi:")
+#syntymävuosi = input("Anna vuosi:")
+#print(f'Hei {nimi}', olet syntynyt eli  "1990")
+
+osa1 = input("Anna 1. osa:- ")
+osa2 = input("Anna 1. osa:- ")
+osa3 = input("Anna 1. osa:- ")
+print("osa1"+"-"+"osa2"+"-"+"osa3")
+("simhala"+"-"+"bimsala"+"-"+"bim!")
