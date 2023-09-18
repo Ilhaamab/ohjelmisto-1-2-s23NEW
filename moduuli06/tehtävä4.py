d684f9d7213ba2150c0e0fea9956304a02f81e19
Ilhaam <ilhaamn1@gmail.com>
2023-09-18 11:07:49 +0300

    moduuli07

--- moduuli06/tehtävä4.py
+++ moduuli06/tehtävä4.py
@@ -3,4 +3,8 @@
 # Kirjoita testausta varten pääohjelma,
 # jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
 
-def summa
+def whole_numerot(int):return sum(int)
+
+int_list = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+
+print(whole_numerot (int_list))
