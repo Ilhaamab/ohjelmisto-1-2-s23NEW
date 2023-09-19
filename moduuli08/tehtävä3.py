7de43fdadd22d73a1c9eb2f076fa6e50e5e294eb
Ilhaam <ilhaamn1@gmail.com>
2023-09-19 10:15:43 +0300

    moduuli08,tehtävä3.py

--- moduuli08/tehtävä3.py
+++ moduuli08/tehtävä3.py
@@ -1,5 +1,6 @@
-import mysql.connestor
-from geopy.distanceimport goodesic
+import mysql.connector
+from geopy.distance
+import geodesic
 
 def search_airport(ident):
     sql="SELECT latitude_deg, longitude_deg FROM airport WEHERIDENT = %S;"
@@ -15,3 +16,8 @@ connection = mysql.connector.connect(
          password='Lontoo2023',
          autocommit=True
 
+airport1 = input('Enter the ICAO-codeof first airport:')
+airport2 = input('Enter the ICAO-code of the second airport:')
+distance = geodesic(args; search_airport(airport1),search_airport(airport2)).kilometers
+
+print(f'The distance between the airports is(distance:.0f) kilometers.')
